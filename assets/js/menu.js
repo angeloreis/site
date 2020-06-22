785a342b42c2703e099399047d0ebf1ce11fcd27
Angelo Reis <angelo.desenvolvedor@gmail.com>
2020-06-22 20:30:01 -0300

    CSS mais completo

---
+++ assets/js/menu.js
@@ -0,0 +1,6 @@
+var alternador = document.querySelector(".js-btn-chaveador");
+
+alternador.onclick = function () {
+  var menu = document.querySelector(".js-menu-mobile");
+  menu.classList.toggle("menu--display-mobile");
+};
