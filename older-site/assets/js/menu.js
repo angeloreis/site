303be08e2c1e2649ebc2b446ce3c11be03147432
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-13 11:48:33 -0300

    fix: remove garbage

--- older-site/assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-var alternador = document.querySelector(".js-btn-chaveador");
-
-alternador.onclick = function () {
-  var menu = document.querySelector(".js-menu-mobile");
-  menu.classList.toggle("menu--display-mobile");
-};
