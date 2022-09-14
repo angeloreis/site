c5b69b4813eda3f243c060918da04cc7335d1492
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-14 16:10:14 -0300

    fix: add title pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ import Document, {
       return (
         <Html>
           <Head lang="pt-BR">
+                <title>Angelo Reis - FrontEnd</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"/>
           </Head>
