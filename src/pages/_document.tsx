a6d8007f61d024ce344b13d84df77ce3ff9b2b68
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-17 19:02:46 -0300

    fix: generate a favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,10 @@ import Document, {
           <Head lang="pt-BR" title='Angelo Reis - FrontEnd'>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
           </Head>
           <body>
             <Main />
