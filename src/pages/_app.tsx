f5843690e37df4373487dece725fdd74372d2c57
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-17 18:50:42 -0300

    fix: header title pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
     <ChakraProvider>
+      <Head>
+        <title>Angelo Reis - FrontEnd</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>    
+    </>
   )
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,8 +11,7 @@ import Document, {
       render() {
       return (
         <Html>
-          <Head lang="pt-BR">
-                <title>Angelo Reis - FrontEnd</title>
+          <Head lang="pt-BR" title='Angelo Reis - FrontEnd'>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"/>
           </Head>
