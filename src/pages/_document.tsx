c4c47c7472fedeacaef54ed5e03d11e8b8489158
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-17 19:07:29 -0300

    fix: add meta tags at header site

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,9 @@ import Document, {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
+                <meta name="description" content="Site do Desenvolvedor Angelo Reis" />
+                <meta name="keywords" content="HTML, CSS, JavaScript, ReactJS, NodeJS, ORM" />
+                <meta name="author" content="Angelo Reis" />
           </Head>
           <body>
             <Main />
