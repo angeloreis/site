c0aca93c5adc9266c04b2ef1c4b79a8e906bfc9e
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-12-01 03:31:41 -0300

    fix: add developer name on header site

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
 import { Menu } from "../Menu"
-import { Flex } from '@chakra-ui/react'
+import { Flex, Text, useMediaQuery } from '@chakra-ui/react'
 
 export function Header() {
     return (
         <Flex as='header' flexDirection="row" alignItems="center" justifyContent="space-between" paddingX="30px" minH="97px" w="100%">
-            <div></div>
-            <Menu />
+                    <Text fontSize={"2rem"} fontWeight="extrabold">ANGELO REIS</Text>
+                    <Menu />    
         </Flex>
     )
 }
