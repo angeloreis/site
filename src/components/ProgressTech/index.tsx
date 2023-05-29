03632cabcf38ead0cc117eb77cc21b5d150c0dd9
Angelo Reis <angelo.desenvolvedor@gmail.com>
2023-05-28 21:19:44 -0300

    fix(sections): use Progress line component

--- src/components/ProgressTech/index.tsx
+++ src/components/ProgressTech/index.tsx
@@ -1,19 +1,30 @@
-import { Flex, Heading, CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
+import {
+  Heading,
+  Progress
+} from "@chakra-ui/react";
 
 interface ProgressTechProps {
-    techName: string
-    valueOfProgress: number
-    colorProgress: string
-    isMobile?: boolean
+  techName: string;
+  valueOfProgress: number;
+  colorProgress: string;
+  isMobile?: boolean;
 }
 
-export function ProgressTech({ techName, valueOfProgress, colorProgress = 'orange.500', isMobile }: ProgressTechProps) {
-    return (
-        <Flex flexDirection="column" alignItems="center" justify={"center"} borderRadius="7px" background="blackAlpha.500" padding="6px" color="white">
-            <CircularProgress value={valueOfProgress} color={colorProgress} size={isMobile ? "74px" : "98px"} thickness='4px' >
-                <CircularProgressLabel>{valueOfProgress.toString().concat('%')}</CircularProgressLabel>
-            </CircularProgress>
-            <Heading size={isMobile ? "xs" : "sm"}>{techName}</Heading>
-        </Flex>
-    )
+export function ProgressTech({
+  techName,
+  valueOfProgress,
+  colorProgress = "orange.500",
+  isMobile,
+}: ProgressTechProps) {
+  return (
+    <>
+      <Heading size={isMobile ? "xs" : "sm"} color="whiteAlpha.900">{techName}</Heading>
+      <Progress
+        colorScheme={colorProgress}
+        value={valueOfProgress}
+        height=".6rem"
+        size="md"
+      />
+    </>
+  );
 }
